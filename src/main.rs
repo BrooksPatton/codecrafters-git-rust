052d3c8514175320af6ce54f1133910fbebfcdf8
Brookzerker <brooks@brookzerker.com>
2023-12-21 07:55:30 -0700

    got the folder name and file names

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,16 @@ fn main() {
         fs::create_dir(".git/objects").unwrap();
         fs::create_dir(".git/refs").unwrap();
         fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-        println!("Initialized git directory")
+        println!("Initialized git directory");
+    } else if args[1] == "cat-file" {
+        if args[2] == "-p" {
+            let hash = args[3].clone();
+            // the first two characters of the hash are the folder name, and the rest are the file name
+            let folder_name = &hash[0..2];
+            let file_name = &hash[2..];
+            dbg!(folder_name, file_name);
+        }
     } else {
-        println!("unknown command: {}", args[1])
+        println!("unknown command: {}", args[1]);
     }
 }
